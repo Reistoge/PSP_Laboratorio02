35ab0dfb46c13fd392118bf00387967b4dc888fb
midnighght <msarnojimenez+github@gmail.com>
2025-05-29 16:19:57 -0400

    attack test

--- src/main/java/ui/LoginController.java
+++ src/main/java/ui/LoginController.java
@@ -4,7 +4,7 @@ public class LoginController {
     private final AuthService authService = new AuthService();
     //3. Dependencia cruzada en capa ui accediendo a la clase UserView
     // UserView -> MainController-> LoginController -> UserView
-    //ui.UserView userView = new ui.UserView();
+    ui.UserView userView = new ui.UserView();
     public boolean login(String username, String password) {
         return authService.authenticate(username, password);
     }
